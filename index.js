91297e2004f09e2966c6afaa6cf5ec63b4d1b518
Simon Kiflay <szkiflay@gmail.com>
2018-08-10 16:12:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var animal= 'cat';
-  console.log(animal);
+  return animal;
 }
 yourAnimal();
 animal;
